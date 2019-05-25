cffa521663cec1d22d50bc0d57bc4d2b10951fc8
Flávio Tassan <tassanf@gmail.com>
2019-05-25 17:26:53 -0300

    simplifying the code v1

--- lib/fizz_buzzer.ex
+++ lib/fizz_buzzer.ex
@@ -18,14 +18,9 @@ defmodule FizzBuzzer do
     end
   end
 
-  @spec multipleOf(integer, integer) :: integer
-  def multipleOf(number, multiple) do
-    number - div(number, multiple) * multiple
-  end
-
   @spec is_multiple(integer, integer) :: boolean
   def is_multiple(number, multiple) do
-    if multipleOf(number, multiple) == 0 do true else false end
+    if rem(number, multiple) == 0 do true else false end
   end
 
 end
