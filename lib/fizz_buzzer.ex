06f81e80216a783d7f6cc721637fe93f742d740b
Flávio Tassan <tassanf@gmail.com>
2019-05-25 17:02:01 -0300

    multiples of 3 and 5

--- lib/fizz_buzzer.ex
+++ lib/fizz_buzzer.ex
@@ -2,10 +2,17 @@ defmodule FizzBuzzer do
 
   @spec fizzbuzz(integer()) :: <<_::8>>
   def fizzbuzz(number) do
-    case number do
-      1 -> "1"
-      2 -> "2"
-      4 -> "4"
-    end
+      case number do
+        5 -> "buzz"
+        10 -> "buzz"
+        25 -> "buzz"
+        3 -> "fizz"
+        6 -> "fizz"
+        9 -> "fizz"
+        1 -> "1"
+        2 -> "2"
+        4 -> "4"
+      end
   end
+
 end
--- test/fizz_buzzer_test.exs
+++ test/fizz_buzzer_test.exs
@@ -8,4 +8,17 @@ defmodule FizzBuzzerTest do
     assert FizzBuzzer.fizzbuzz(4) == "4"
 
   end
+
+  test "multiples of 3" do
+    assert FizzBuzzer.fizzbuzz(3) == "fizz"
+    assert FizzBuzzer.fizzbuzz(6) == "fizz"
+    assert FizzBuzzer.fizzbuzz(9) == "fizz"
+  end
+
+  test "multiples of 5" do
+    assert FizzBuzzer.fizzbuzz(5) == "buzz"
+    assert FizzBuzzer.fizzbuzz(10) == "buzz"
+    assert FizzBuzzer.fizzbuzz(25) == "buzz"
+  end
+
 end
